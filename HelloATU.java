05108c646a165a6128c58e4b699f75b02c3c9fe3
Dmytro Arkhypenko - STUDENT <l00194711@atu.ie>
2026-01-28 11:25:07 +0000

    HelloATU.java added

---
+++ HelloATU.java
@@ -0,0 +1,6 @@
+public class HelloATU {
+    public static void main(String[] args) {
+        // Prints "Hello, ATU from Dmytro" as output
+        System.out.println("Hello, ATU from Dmytro");
+    }
+}
